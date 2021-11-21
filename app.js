344610d548909aa21bd95d22d8b157ac042e574a
rvp39 <vipulk3009@mail.com>
2021-11-21 10:31:27 +0530

    server code modified

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require("express");
 var app = express();
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 var bodyParser = require("body-parser");
+const csrfMiddleware = csrf({ cookie: true });
 
+app.use(cookieParser());
+app.use(csrfMiddleware);
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("Views"))
-app.set("view engine","ejs")
+app.use(express.static("static"));
+app.set("view engine","ejs");
 
 
-
+app.all("*", (req, res, next) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    next();
+  });
 
 
 
